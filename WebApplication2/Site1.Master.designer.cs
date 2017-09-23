93f511622754c3a3691b74a1c2c98e883183d81d
chintan21 <cdabhi37@gmail.com>
2017-09-24 00:19:49 +0530

    Final for PPT presentation

--- WebApplication2/Site1.Master.designer.cs
+++ WebApplication2/Site1.Master.designer.cs
@@ -202,15 +202,6 @@ namespace WebApplication2 {
         protected global::System.Web.UI.WebControls.TextBox TextBox11;
         
         /// <summary>
-        /// RegularExpressionValidatorpassword control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidatorpassword;
-        
-        /// <summary>
         /// TextBox12 control.
         /// </summary>
         /// <remarks>
